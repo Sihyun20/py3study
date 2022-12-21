12737873176b4551dbd5850f877c30589bc2d1b8
monjimolainga <leeha223@gmail.com>
2022-12-21 20:17:46 +0900

    답을 좀 더 찾은 코드로 업데이트 주석도 업데이트

--- 20221220/Lee/test1130.py
+++ 20221220/Lee/test1130.py
@@ -37,7 +37,6 @@ import os
 path = os.getcwd();
 path
 os.mkdir("temp")
-word = input("입력하세요: ")
 outfp = open("temp/indata.txt","w")
 while True :
     outstr = input("내용입력=>")
@@ -46,7 +45,7 @@ while True :
     outfp.writelines(outstr+"\n")
 outfp.close() 
 
-
+#업로드 후 필요없는부분이 있어서 수정함.
 
 
 
@@ -90,6 +89,24 @@ else:
 ###위에서 copy경로에 이름을 써줬기 때문에 그 이름과 동일하게 복사본파일 이름을 입력하면 
 ###파일이 그 경로에 존재해서 그다음 프린트인 복사완료가 나옴..
 
+import os
+name = input("원본파일의 이름을 입력하세요 : ")
+if os.path.exists(name):
+     du_name = input("복사본 파일의 이름을 입력하세요 : ")
+     ori = open(name, "r", encoding = "UTF-8")
+     dup = open(du_name, "w", encoding = "UTF-8")
+     while True:
+         indata = ori.read()
+         if not indata:
+             break
+         dup.write(indata)
+     ori.close()
+     dup.close()
+     print("복사 완료")
+else : 
+     print("원본파일이 존재하지 않습니다.")
+###시현이 답이 생각에 흐름과 동일하게 가서 적당하고 딱 좋음!
+###해결못했던 복사가 먼저되는 것을 이 코드로는 간단하게 해결 좋아~    
 
 
 
@@ -128,10 +145,23 @@ number= [["*** ",
 		 ["*** ","* * ","*** ","  * ","  * "]
 		]
 
-inNum = int(input("숫자를 입력하세요 =>"))
+inNum = input("숫자를 입력하세요 =>")
+num = [i for i in inNum]
+num = list(map(int,num))
+
+for i in range(5):
+    for j in range(len(num)):
+        print(number[num[j]][i], end="")
+    print()
+
+
+
 ###뭔가 반복해서 입력값에 맞춰 출력되야되는데 
 ###어떻게 식을 세워야될지 모르겠음....넘어렵
-
+###=>
+###표를 만들어서 넣어보거나 노트에 적어봐서 어떻게 나와야되는지 눈으로
+###확연히 보이도록 해서 풀어보자(사부의 팁)
+###input받을때 int로 받으면 인덱스로 뽑아와서 하는게 안됨 그부분 유의
 
 
 
@@ -193,14 +223,35 @@ while True :
         break
     print()
     print(row)
+cur.close()
 conn.close()
 
 ###sql과 파이썬을 어떻게 혼합해서 써야 출력이 되는지...
 ### sql구문 실행하는 명령문을 변수에 넣을 수 있는건지..
 ###할수있는데까지해봄.. 근데 이거저거 조합하다보니 엉망인것같음..
 
+import sqlite3
+conn = sqlite3.connect("mydb") 
+cur = conn.cursor()
+while True :
+    sql = input("sql 입력하세요=========\n")
+    if sql=="" :
+        break
+    cur.execute(sql)
+    rows = cur.fetchall()
+    if len(rows) > 0 : #select 구문인 경우
+       print()
+       print("조회 레코드수:",len(rows),",조회 컬럼수:",len(rows[0]))    
+       print()
+       for row in rows :
+           print(row)
+cur.close()
+conn.close();
 
-
+###위는 예시답인데,
+###생각보다 답을 보니 간단했는데 len, for, if를 잘 사용해야겠다.
+###내가 푼 방식?을 어떻게 정리해야 오류없이 문제의 답으로 향할 수 있는지
+###고민이 더 필요하다.
 
 
 
